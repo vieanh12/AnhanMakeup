91417050e48b78dc5e90867818245a9be0e26f94
vieanh12 <anhtvth2302006@fpt.edu.vn>
2024-05-18 12:39:09 +0700

    d2

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var navbarLinks = document.getElementById("navbarLinks");
+    if (navbarLinks.style.display === "block") {
+        navbarLinks.style.display === "none";
+    } else {
+        navbarLinks.style.display = "block";
+    }
+}
